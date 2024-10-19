be04cd5fa47f6b0ecd60df7a2468de52ebe01a92
Omp200g <omp2g@mtmail.mtsu.edu>
2024-10-19 01:52:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
 import bcrypt  # Importing bcrypt for password hashing
+#pip install Flask
+#pip install mysql-connector-python
+#pip install bcrypt
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Needed to keep sessions secure
